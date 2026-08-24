d281722f8d740b1b8ee87fec84854bc6dc517b13
kwb130212-discord <kwb130212@gmail.com>
2026-08-24 21:53:33 +0900

    feat: install Dyno-style server picker

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from ticket_control import install as install_ticket_control
 from persistent_settings import install as install_persistent_settings
 from dashboard_shortcuts import install as install_dashboard_shortcuts
 from webboard_features_v3 import install as install_webboard_features
+from dashboard_servers_v2 import install as install_dashboard_servers
 
 install_startup_fixes(core)
 install_web_entry(core)
@@ -27,6 +28,7 @@ install_ticket_control(core)
 install_persistent_settings(core)
 install_dashboard_shortcuts(core)
 install_webboard_features(core)
+install_dashboard_servers(core)
 
 app = core.app
 bot = core.bot
